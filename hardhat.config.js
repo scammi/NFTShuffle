2b148b7ba8414cc5fd5d84237bb70487da1c936c
scammi <scammi@gmail.com>
2022-09-22 17:28:13 -0300

    replace npm with yarn and added hardhat deploy depency

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
