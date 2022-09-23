10e12199d97d386afbc014bad46a68e36a5acdde
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2022-09-23 19:36:37 -0300

    Remove code from ShuffleNFT.

--- contracts/ShuffleNFT.sol
+++ contracts/ShuffleNFT.sol
@@ -4,15 +4,27 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ShuffleNFT is ERC721{
+// import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract ShuffleNFT is ERC721 {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Random NFT", "rNFT"){}
+    constructor(address vrfCoordinator)
+        ERC721("Random NFT", "rNFT")
+        // VRFConsumerBaseV2(vrfCoordinator)
+    {
+
+    }
 
     uint256[] public shuffle;
     uint256 public entropy = block.timestamp;
+
+    // function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+
+    //     entropy = randomWords[0];
+    // }
 
 
     function mint() public {
@@ -20,10 +32,16 @@ contract ShuffleNFT is ERC721{
 
         uint256 newTokenId = _tokenIdCounter.current();
 
+        // FIXME : This should NOT mint the token to the user.
+        //         Instead it should assign an index to `msg.sender` to later shuffle the array, 
+        //         and then allow users to mint their token with the shuffled id. 
         _mint(msg.sender, newTokenId);
 
         shuffle.push(newTokenId);
     }
+
+
+
 
 
     function shuffleArray() public {
