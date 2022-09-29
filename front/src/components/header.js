42781fa2194d79448891ed4bb59e271990cf7c54
scammi <scammi@gmail.com>
2022-09-29 20:15:32 -0300

    Simple react front end in gatsby

---
+++ front/src/components/header.js
@@ -0,0 +1,40 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { Button } from "@mui/material"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      margin: `0 auto`,
+      padding: `var(--space-4) var(--size-gutter)`,
+      display: `flex`,
+      alignItems: `center`,
+      justifyContent: `space-between`,
+    }}
+  >
+    <Link
+      to="/"
+      style={{
+        fontSize: `var(--font-sm)`,
+        textDecoration: `none`,
+      }}
+    >
+      {siteTitle}
+    </Link>
+    <Button>
+      Connect wallet
+    </Button>
+
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
