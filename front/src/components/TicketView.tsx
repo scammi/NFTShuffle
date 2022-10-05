27140cdb7da6fecdb111c7c6c14c703f339be640
scammi <scammi@gmail.com>
2022-10-05 01:23:47 -0300

    Mint component

--- front/src/components/TicketView.tsx
+++ front/src/components/TicketView.tsx
@@ -12,47 +12,57 @@ const TicketView = ({}: Props) => {
   const shuffleOne = useShuffleOne();
 
   const [ canBuy, setCanBuy ] = useState(false);
+  const [ canMint, setCanMint ] = useState(false);
+
   const [ buyTransaction, setBuyTransaction ] = useState();
+  const [ mintTransaction, setMintTransaction ] = useState();
 
   useEffect(() => {
     (async()=>{
       const isOpen = await shuffleOne.isRaffleOpen();
       const maxPerAddress = await shuffleOne.MAX_PER_ADDRESS();
       const ticketsBough = await shuffleOne.participants(web3.wallet);
-    
-      if (!isOpen && (maxPerAddress.gt(ticketsBough.ownedTickets))) {
-        setCanBuy(true);
-      } else {
-        setCanBuy(false);
-      }
-    })();
-  }, [ buyTransaction ] );
 
-  const BuyTicket = () => {
-    // todo check if user bough ticket;
+      // Can buy ?
+      if (!isOpen && (maxPerAddress.gt(ticketsBough.ownedTickets))) { setCanBuy(true); }
+
+      // Can mint ?
+      if (isOpen && (ticketsBough.minted.lt(ticketsBough.ownedTickets))) { setCanMint(true) };
+    })();
+  }, [ buyTransaction, mintTransaction ] );
+
+  const BuyTicketButton = () => {
     const buyTicketHandle = async () => {
       try {
         const response = await shuffleOne.buyTicket({ value: ethers.utils.parseEther("0.2") });
         const receipt = await response.wait();
         setBuyTransaction(receipt);
-        console.log(receipt);
       } catch (e) {
         console.log(e);
       }
     };
-  
     return (
-      <>
-       <Button onClick={() => { buyTicketHandle() }}> Buy ticket </Button>
-      </>
+     <Button onClick={() => { buyTicketHandle() }}> Buy ticket </Button>
     );
   };
 
-  if (canBuy) {
-   return <BuyTicket /> 
-  } else {
-    return 'Mint token'
-  }
+  const MintTokenButton = () => {
+    const mintTokenHandle = async () => {
+      try {
+        const response = await shuffleOne.mint();
+        const receipt = await response.wait();
+        setMintTransaction(receipt);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    return (
+     <Button onClick={() => { mintTokenHandle() }}> Mint token </Button>
+    ); 
+  };
+
+  if (canBuy) { return <BuyTicketButton /> }
+  else if (canMint) { return  <MintTokenButton /> }
 };
 
 export { TicketView };
