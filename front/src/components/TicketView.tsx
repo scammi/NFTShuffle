2298227c4858ad3392ceb29cc31daa5c3acd1b08
scammi <scammi@gmail.com>
2022-10-05 01:00:48 -0300

    update buy state after buying

--- front/src/components/TicketView.tsx
+++ front/src/components/TicketView.tsx
@@ -1,23 +1,40 @@
-import React from "react";
-import { useIsRaffleOpen } from "../hooks/useIsRaffleOpen"
+import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import { useShuffleOne } from "../hooks/useShuffleOne";
 
 import { ethers } from "ethers"
+import { useWeb3Context } from "../context/Web3";
 
 type Props = {}
 
 const TicketView = ({}: Props) => {
-  const isOpen = useIsRaffleOpen();
+  const [ web3 ] = useWeb3Context();
+  const shuffleOne = useShuffleOne();
+
+  const [ canBuy, setCanBuy ] = useState(false);
+  const [ buyTransaction, setBuyTransaction ] = useState();
+
+  useEffect(() => {
+    (async()=>{
+      const isOpen = await shuffleOne.isRaffleOpen();
+      const maxPerAddress = await shuffleOne.MAX_PER_ADDRESS();
+      const ticketsBough = await shuffleOne.participants(web3.wallet);
+    
+      if (!isOpen && (maxPerAddress.gt(ticketsBough.ownedTickets))) {
+        setCanBuy(true);
+      } else {
+        setCanBuy(false);
+      }
+    })();
+  }, [ buyTransaction ] );
 
   const BuyTicket = () => {
-    const shuffleOne = useShuffleOne();
-    
     // todo check if user bough ticket;
     const buyTicketHandle = async () => {
       try {
         const response = await shuffleOne.buyTicket({ value: ethers.utils.parseEther("0.2") });
         const receipt = await response.wait();
+        setBuyTransaction(receipt);
         console.log(receipt);
       } catch (e) {
         console.log(e);
@@ -31,7 +48,7 @@ const TicketView = ({}: Props) => {
     );
   };
 
-  if (isOpen) {
+  if (canBuy) {
    return <BuyTicket /> 
   } else {
     return 'Mint token'
--- front/src/hooks/useIsRaffleOpen.tsx
+++ front/src/hooks/useIsRaffleOpen.tsx
@@ -1,28 +1,20 @@
+import { Transaction } from "ethers";
 import React, { useEffect, useState } from "react";
 import { useWeb3Context } from "../context/Web3";
 import { useShuffleOne } from "./useShuffleOne";
 
+type Props = { 
+  update?: Transaction
+}
 // todo rename to can buy
-const useIsRaffleOpen = () => {
+const useIsRaffleOpen = (Props: Props ) => {
   const [ web3 ] = useWeb3Context();
   const shuffleOne = useShuffleOne();
   const [ canBuy, setCanBuy ] = useState(false);
 
   useEffect(() => {
-    (async()=>{
-      const isOpen = await shuffleOne.isRaffleOpen();
-      const maxPerAddress = await shuffleOne.MAX_PER_ADDRESS();
-      const ticketsBough = await shuffleOne.participants(web3.wallet);
 
-      console.log(!isOpen, ticketsBough.ownedTickets,maxPerAddress, maxPerAddress.gte(ticketsBough.ownedTickets));
-
-      if (!isOpen && (maxPerAddress.gt(ticketsBough.ownedTickets))) {
-        setCanBuy(true);
-      } else {
-        setCanBuy(false);
-      }
-    })();
-  },[]);
+  }, [ Props.update ]);
 
   return canBuy;
 };
