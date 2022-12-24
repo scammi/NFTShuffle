d4ca73663a8100cd1c0ca17f6967147f16b129d7
scammi <scammi@gmail.com>
2022-12-24 07:15:15 -0300

    hover papaer btn

--- front/src/components/PaperFloat.tsx
+++ front/src/components/PaperFloat.tsx
@@ -6,7 +6,17 @@ import { Paper } from "@mui/material";
 
 const PaperFloat = ({ children  }) => {
   return (
-    <Paper elevation={3} >
+    <Paper 
+      sx={{
+        elevation: 2,
+        borderRadius: 2,
+        boxShadow: 1,
+        ':hover' : {
+          boxShadow: 3,
+          elevation: 5,
+        }
+      }}
+    >
       {children}
     </Paper>
   );
