e81fca70cf62e53be93f9dc21c74f865a0adbd52
scammi <scammi@gmail.com>
2022-12-26 11:46:37 -0300

    Updates

---
+++ component---src-pages-404-js-2630a6088df3877f40b0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(t,e,n){n.r(e),n.d(e,{Head:function(){return s}});var a=n(7294),u=n(6860),l=n(9357);const s=()=>a.createElement(l.Z,{title:"404: Not Found"});e.default=()=>a.createElement(u.Z,null,a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-2630a6088df3877f40b0.js.map
