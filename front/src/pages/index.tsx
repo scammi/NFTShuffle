2150536bb53316a6121775ca05b2f8ab62be9101
scammi <scammi@gmail.com>
2022-12-26 12:03:18 -0300

    spacing for about

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -77,7 +77,15 @@ const IndexPage = () => {
         <Typography variant="h4" sx={{ textAlign: 'center' }} > About Shuffle </Typography>
         <PaperFloat sxPaper={{ padding: 2 }}>
           <Typography variant="body1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </Typography>
+          <br/>
+          <Typography variant="body1">
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          </Typography>
+          <br/>
+          <Typography variant="body1">
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
           </Typography>
         </PaperFloat>
       </Box>
